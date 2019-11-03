edaa963b874c484018598369822ad6da7d0cddae
Tim Dumol <tim@timdumol.com>
2019-11-03 20:29:32 +0800

    Bump to 1.0.0. Use modern deps and code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,60 @@
 #![crate_name="otp"]
 #![crate_type="lib"]
 
-extern crate openssl;
-extern crate serialize;
-extern crate base32;
-extern crate time;
+extern crate ring;
+extern crate data_encoding;
 
-use time::get_time;
-use openssl::crypto::hash::HashType;
-use openssl::crypto::hmac::HMAC;
+use std::time::{SystemTime, SystemTimeError};
+use std::convert::TryInto;
+use data_encoding::{BASE32_NOPAD, DecodeError};
+use ring::hmac;
 
-use std::io::extensions::{u64_to_be_bytes, u64_from_be_bytes};
+#[derive(Debug)]
+pub enum TOTPError {
+    DecodeError(DecodeError),
+    SystemTimeError(SystemTimeError),
+    InvalidDigest
+}
 
 /// Decodes a secret (given as an RFC4648 base32-encoded ASCII string)
 /// into a byte string
-fn decode_secret(secret: &[Ascii]) -> Option<Vec<u8>> {
-    base32::decode(base32::Base32Type::UnpaddedRFC4648Base32, secret)
+fn decode_secret(secret: &str) -> Result<Vec<u8>, DecodeError> {
+    BASE32_NOPAD.decode(secret.as_bytes())
 }
 
 /// Calculates the HMAC digest for the given secret and counter.
-fn calc_digest(decoded_secret: &[u8], counter: u64) -> Vec<u8> {
-    let mut hmac = HMAC(HashType::SHA1, decoded_secret);
-    u64_to_be_bytes(counter, 8, |bytes| {
-        hmac.update(bytes);
-    });
-
-    let rv = hmac.finalize();
-    println!("digest {}", rv);
-    rv
+fn calc_digest(decoded_secret: &[u8], counter: u64) -> hmac::Tag {
+    let key = hmac::Key::new(hmac::HMAC_SHA1_FOR_LEGACY_USE_ONLY, decoded_secret);
+    hmac::sign(&key, &counter.to_be_bytes())
 }
 
 /// Encodes the HMAC digest into a 6-digit integer.
-fn encode_digest(digest: &[u8]) -> u32 {
-    let offset = *digest.last().unwrap() as uint & 0xf;
-    let code = u64_from_be_bytes(digest, offset, 4) as u32;
-
-    (code & 0x7fffffff) % 1_000_000
+fn encode_digest(digest: &[u8]) -> Result<u32, TOTPError> {
+    let offset = match digest.last() {
+        Some(x) => *x & 0xf,
+        None => return Err(TOTPError::InvalidDigest)
+    } as usize;
+    let code_bytes = match digest[offset..offset+4].try_into() {
+        Ok(x) => x,
+        Err(_) => return Err(TOTPError::InvalidDigest)
+    };
+    let code = u32::from_be_bytes(code_bytes);
+    Ok((code & 0x7fffffff) % 1_000_000)
 }
 
 /// Performs the [HMAC-based One-time Password Algorithm](http://en.wikipedia.org/wiki/HMAC-based_One-time_Password_Algorithm)
 /// (HOTP) given an RFC4648 base32 encoded secret, and an integer counter.
-pub fn make_hotp(secret: &[Ascii], counter: u64) -> Option<u32> {
-    decode_secret(secret).map(|decoded| {
-        encode_digest(calc_digest(decoded.as_slice(), counter).as_slice())
-    })
+pub fn make_hotp(secret: &str, counter: u64) -> Result<u32, TOTPError> {
+    let decoded = match decode_secret(secret) {
+        Ok(x) => x,
+        Err(err) => return Err(TOTPError::DecodeError(err))
+    };
+    encode_digest(calc_digest(decoded.as_slice(), counter).as_ref())
 }
 
 /// Helper function for `make_totp` to make it testable. Note that times
 /// before Unix epoch are not supported.
-fn make_totp_helper(secret: &[Ascii], time_step: u64, skew: i64, time: u64) -> Option<u32> {
+fn make_totp_helper(secret: &str, time_step: u64, skew: i64, time: u64) -> Result<u32, TOTPError> {
     let counter = ((time as i64 + skew) as u64) / time_step;
     make_hotp(secret, counter)
 }
@@ -56,9 +62,16 @@ fn make_totp_helper(secret: &[Ascii], time_step: u64, skew: i64, time: u64) -> O
 /// Performs the [Time-based One-time Password Algorithm](http://en.wikipedia.org/wiki/Time-based_One-time_Password_Algorithm)
 /// (TOTP) given an RFC4648 base32 encoded secret, the time step in seconds,
 /// and a skew in seconds.
-pub fn make_totp(secret: &[Ascii], time_step: u64, skew: i64) -> Option<u32> {
-    let now = get_time();
-    make_totp_helper(secret, time_step, skew, now.sec as u64)
+pub fn make_totp(secret: &str, time_step: u64, skew: i64) -> Result<u32, TOTPError> {
+    let now = SystemTime::now();
+    let time_since_epoch = match now.duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(t) => t,
+        Err(err) => return Err(TOTPError::SystemTimeError(err))
+    };
+    match make_totp_helper(secret, time_step, skew, time_since_epoch.as_secs() ) {
+        Ok(d) => Ok(d),
+        Err(err) => return Err(err)
+    }
 }
 
 #[cfg(test)]
@@ -67,16 +80,16 @@ mod tests {
 
     #[test]
     fn hotp() {
-        assert_eq!(make_hotp("base32secret3232".to_ascii(), 0), Some(260182));
-        assert_eq!(make_hotp("base32secret3232".to_ascii(), 1), Some(55283));
-        assert_eq!(make_hotp("base32secret3232".to_ascii(), 1401), Some(316439));
+        assert_eq!(make_hotp("BASE32SECRET3232", 0).unwrap(), 260182);
+        assert_eq!(make_hotp("BASE32SECRET3232", 1).unwrap(), 55283);
+        assert_eq!(make_hotp("BASE32SECRET3232", 1401).unwrap(), 316439);
     }
 
     #[test]
     fn totp() {
-        assert_eq!(make_totp_helper("base32secret3232".to_ascii(), 30, 0, 0), Some(260182));
-        assert_eq!(make_totp_helper("base32secret3232".to_ascii(), 3600, 0, 7), Some(260182));
-        assert_eq!(make_totp_helper("base32secret3232".to_ascii(), 30, 0, 35), Some(55283));
-        assert_eq!(make_totp_helper("base32secret3232".to_ascii(), 1, -2, 1403), Some(316439));
+        assert_eq!(make_totp_helper("BASE32SECRET3232", 30, 0, 0).unwrap(), 260182);
+        assert_eq!(make_totp_helper("BASE32SECRET3232", 3600, 0, 7).unwrap(), 260182);
+        assert_eq!(make_totp_helper("BASE32SECRET3232", 30, 0, 35).unwrap(), 55283);
+        assert_eq!(make_totp_helper("BASE32SECRET3232", 1, -2, 1403).unwrap(), 316439);
     }
 }
